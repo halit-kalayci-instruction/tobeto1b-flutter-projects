83164defadb5c1a96a9ee78cca4abfe1e155c098
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-11-09 14:54:25 +0300

    list view

--- expenseapp/lib/main.dart
+++ expenseapp/lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:expenseapp/models/expense.dart';
+import 'package:expenseapp/pages/expenses_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Scaffold()));
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: ExpensesPage(),
+      ),
+    ),
+  );
 }
---
+++ expenseapp/lib/pages/expenses_page.dart
@@ -0,0 +1,77 @@
+import 'dart:math';
+
+import 'package:expenseapp/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class ExpensesPage extends StatefulWidget {
+  const ExpensesPage({Key? key}) : super(key: key);
+
+  @override
+  _ExpensesPageState createState() => _ExpensesPageState();
+}
+
+class _ExpensesPageState extends State<ExpensesPage> {
+  List<Expense> expenses = [
+    Expense(name: "Yemek", price: 500, date: DateTime.now()),
+    Expense(name: "Udemy Kursu", price: 200, date: DateTime.now()),
+    Expense(name: "Udemy Kursu", price: 200, date: DateTime.now()),
+    Expense(name: "Udemy Kursu", price: 200, date: DateTime.now()),
+    Expense(name: "Udemy Kursu", price: 200, date: DateTime.now()),
+    Expense(name: "Udemy Kursu", price: 200, date: DateTime.now()),
+    Expense(name: "Udemy Kursu", price: 200, date: DateTime.now()),
+    Expense(name: "Udemy Kursu", price: 200, date: DateTime.now()),
+    Expense(name: "Udemy Kursu", price: 200, date: DateTime.now()),
+    Expense(name: "Udemy Kursu", price: 200, date: DateTime.now()),
+    Expense(name: "Udemy Kursu", price: 200, date: DateTime.now()),
+    Expense(name: "Udemy Kursu", price: 200, date: DateTime.now()),
+    Expense(name: "Udemy Kursu", price: 200, date: DateTime.now()),
+    Expense(name: "Udemy Kursu", price: 200, date: DateTime.now()),
+    Expense(name: "Udemy Kursu", price: 200, date: DateTime.now()),
+    Expense(name: "Udemy Kursu", price: 200, date: DateTime.now()),
+    Expense(name: "Udemy Kursu", price: 200, date: DateTime.now()),
+    Expense(name: "Udemy Kursu", price: 200, date: DateTime.now()),
+    Expense(name: "Udemy Kursu", price: 200, date: DateTime.now()),
+    Expense(name: "Udemy Kursu", price: 200, date: DateTime.now()),
+    Expense(name: "Udemy Kursu", price: 200, date: DateTime.now()),
+    Expense(name: "Udemy Kursu", price: 200, date: DateTime.now()),
+    Expense(name: "Udemy Kursu", price: 200, date: DateTime.now()),
+    Expense(name: "Udemy Kursu", price: 200, date: DateTime.now()),
+    Expense(name: "Udemy Kursu", price: 200, date: DateTime.now()),
+    Expense(name: "Udemy Kursu", price: 200, date: DateTime.now()),
+    Expense(name: "Udemy Kursu", price: 200, date: DateTime.now()),
+    Expense(name: "Udemy Kursu", price: 200, date: DateTime.now()),
+    Expense(name: "Udemy Kursu", price: 200, date: DateTime.now()),
+    Expense(name: "Udemy Kursu", price: 200, date: DateTime.now()),
+    Expense(name: "Udemy Kursu", price: 200, date: DateTime.now()),
+    Expense(name: "Udemy Kursu", price: 200, date: DateTime.now()),
+    Expense(name: "Udemy Kursu", price: 200, date: DateTime.now()),
+    Expense(name: "Udemy Kursu", price: 200, date: DateTime.now()),
+    Expense(name: "Udemy Kursu", price: 200, date: DateTime.now()),
+    Expense(name: "Udemy Kursu", price: 200, date: DateTime.now()),
+    Expense(name: "Udemy Kursu", price: 200, date: DateTime.now()),
+    Expense(name: "Udemy Kursu", price: 200, date: DateTime.now()),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        const SizedBox(
+          height: 150,
+          child: Text("Grafik Bölümü"),
+        ),
+        Expanded(
+          child: ListView.builder(
+              itemCount: expenses.length,
+              itemBuilder: (context, index) {
+                return Text(expenses[index].name);
+              }),
+        ),
+        const SizedBox(
+          height: 150,
+          child: Text("Burası bottom bar."),
+        )
+      ]),
+    );
+  }
+}
