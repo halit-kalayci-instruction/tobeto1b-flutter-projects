f8afa53fff0075bed32296f7d6dbf35e2ca8839c
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-01-11 15:08:07 +0300

    storage

--- firebaselesson/lib/screens/home.dart
+++ firebaselesson/lib/screens/home.dart
@@ -1,7 +1,12 @@
+import 'dart:io';
+
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 final firebaseAuthInstance = FirebaseAuth.instance;
+final firebaseStorageInstance = FirebaseStorage.instance;
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -11,6 +16,30 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  File? _selectedImage;
+
+  void _pickImage() async {
+    final image = await ImagePicker().pickImage(source: ImageSource.camera);
+    if (image != null) {
+      setState(() {
+        _selectedImage = File(image.path);
+      });
+    }
+  }
+
+  void _uploadImage() async {
+    if (_selectedImage != null) {
+      // uuid, userid
+      final storageRef =
+          firebaseStorageInstance.ref().child("images").child("image1.jpg");
+
+      await storageRef.putFile(_selectedImage!);
+
+      final url = await storageRef.getDownloadURL();
+      print(url);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,8 +50,24 @@ class _HomeState extends State<Home> {
             },
             icon: const Icon(Icons.logout))
       ]),
-      body: const Center(
-        child: Text("Ana Sayfa"),
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          CircleAvatar(
+            radius: 40,
+            foregroundImage:
+                _selectedImage != null ? FileImage(_selectedImage!) : null,
+          ),
+          TextButton(
+              onPressed: () {
+                _pickImage();
+              },
+              child: const Text("Resim Seç")),
+          ElevatedButton(
+              onPressed: () {
+                _uploadImage();
+              },
+              child: const Text("Yükle"))
+        ]),
       ),
     );
   }
